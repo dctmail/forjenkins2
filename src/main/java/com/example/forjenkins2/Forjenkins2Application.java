d1c7e1a633f889cfbc734768fc62c9ba66905db9
dct <d.ct@mail.com>
2020-08-20 11:24:39 -0400

    First commit!

---
+++ src/main/java/com/example/forjenkins2/Forjenkins2Application.java
@@ -0,0 +1,13 @@
+package com.example.forjenkins2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Forjenkins2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Forjenkins2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/forjenkins2/Forjenkins2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.forjenkins2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Forjenkins2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
