07e52500865a6ddce59d47a8005f2de51bfdbbd4
dct <d.ct@mail.com>
2020-08-20 14:29:41 -0400

    Third commit for API --- hello2!

--- src/main/java/com/example/forjenkins2/controllers/FirstController.java
+++ src/main/java/com/example/forjenkins2/controllers/FirstController.java
@@ -9,4 +9,9 @@ public class FirstController {
     public String test1(){
         return "Hello World!";
     }
+
+    @GetMapping("/hello2")
+    public String test2(){
+        return "Hello World --- 2 !";
+    }
 }
