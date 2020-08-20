c2a2fb606f02155e884f86e0f67e5bef3229a5a2
dct <d.ct@mail.com>
2020-08-20 12:42:57 -0400

    Second commit for API --- hello1!

---
+++ src/main/java/com/example/forjenkins2/controllers/FirstController.java
@@ -0,0 +1,12 @@
+package com.example.forjenkins2.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+    @GetMapping("/hello1")
+    public String test1(){
+        System.out.println("Hello World!");
+    }
+}
